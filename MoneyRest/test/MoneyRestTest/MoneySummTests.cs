f79c98527534635d7aeae7f81fcedd3b7a21b2e2
Роман Романов <romanovrv@tatintec.ru>
2016-08-03 11:51:24 +0300

    tests

---
+++ MoneyRest/test/MoneyRestTest/MoneySummTests.cs
@@ -0,0 +1,30 @@
+﻿using MoneyRest;
+using Xunit;
+
+namespace MoneyRestTest
+{
+    public class MoneySummTests
+    {
+        [Fact]
+        public void CalculateSummandsTest1()
+        {
+            decimal [] numbers = { 100, 100 };
+            MoneySumm ms = new MoneySumm(numbers);
+
+            ms.CalculateSummands();
+            
+            Assert.Equal(ms.Summands, new decimal[]{100});    
+        }
+
+        [Fact]
+        public void CalculateSummandsTest2()
+        {
+            decimal[] numbers = { 100, 100 };
+            MoneySumm ms = new MoneySumm(numbers);
+
+            ms.CalculateSummands();
+
+            Assert.Equal(ms.Summands, new decimal[] { 100 });
+        }
+    }
+}
